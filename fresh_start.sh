10d7c6cf67204f5c676e84540768c4c04aa5ecdd
Madison Swain-Bowden <bowdenm@spu.edu>
2021-07-02 15:47:27 -0700

    Add build step to fresh start

--- fresh_start.sh
+++ fresh_start.sh
@@ -5,6 +5,7 @@ set -e
 # Tear down existing containers, remove volume
 docker-compose down
 docker volume rm openoversight_postgres || true
+docker-compose build
 
 # Start up and populate fields
 docker-compose run --rm web python ../create_db.py
