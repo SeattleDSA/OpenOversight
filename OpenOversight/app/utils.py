f41cb76781b180547f8857e54be0cdd0eea757a1
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-06 15:08:59 -0700

    Simplify how we scrub exif data

--- OpenOversight/app/utils.py
+++ OpenOversight/app/utils.py
@@ -526,8 +526,9 @@ def upload_image_to_s3_and_store_in_db(image_buf, user_id, department_id=None):
     date_taken = find_date_taken(pimage)
     if date_taken:
         date_taken = datetime.datetime.strptime(date_taken, '%Y:%m:%d %H:%M:%S')
+    scrub_exif(pimage)
     scrubbed_image_buf = BytesIO()
-    scrub_exif(pimage).save(scrubbed_image_buf, image_type)
+    pimage.save(scrubbed_image_buf, image_type)
     scrubbed_image_buf.seek(0)
     image_data = scrubbed_image_buf.read()
     hash_img = compute_hash(image_data)
@@ -569,11 +570,8 @@ def find_date_taken(pimage):
 
 
 # https://gist.github.com/ngtvspc/a686dda375df122ba1a5dd8e6654532b
-def scrub_exif(pimage):
-    image_data = list(pimage.getdata())
-    image_without_exif = Pimage.new(pimage.mode, pimage.size)
-    image_without_exif.putdata(image_data)
-    return image_without_exif
+def scrub_exif(pimage: Pimage.Image):
+    pimage.getexif().clear()
 
 
 def get_officer(department_id, star_no, first_name, last_name):
