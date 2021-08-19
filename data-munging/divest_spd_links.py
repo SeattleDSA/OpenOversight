6bf749229b38c527cfc3ecd2d4bb30538855d798
Madison Swain-Bowden <bowdenm@spu.edu>
2021-08-18 20:38:32 -0700

    Add logging, save off links that don't have badge numbers in OO

--- data-munging/divest_spd_links.py
+++ data-munging/divest_spd_links.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
+import logging
 from pathlib import Path
 
 import click
 import pandas as pd
 
 
+log = logging.getLogger()
+
+
 def main(id_path: Path, link_path: Path, output: Path):
+    log.info("Starting import")
     ids = pd.read_csv(id_path, usecols=["id", "badge number"])
     links = pd.read_csv(link_path)
     # Collapse the multiple links columns into a single column list
@@ -20,10 +25,13 @@ def main(id_path: Path, link_path: Path, output: Path):
     links = links.explode("url", ignore_index=True)
     # Drop any rows where a link doesn't exist (this will be most)
     links = links[links["url"].notna()]
-    # Join the two dataframes on badge number, discard any rows with missing values
-    merged = links.merge(ids, how="inner").astype({"id": pd.Int64Dtype()})[
-        ["id", "url"]
-    ]
+    # Join the two dataframes on badge number
+    merged = links.merge(ids, how="left").astype({"id": pd.Int64Dtype()})
+    # Split off the links that don't have an OpenOversight badge associated with them
+    _has_id = merged["id"].notna()
+    missing = merged[~_has_id]
+    # Remove those missing urls
+    merged = merged[_has_id][["id", "url"]]
     # Rename id to "officer_ids", used in importer:
     # https://openoversight.readthedocs.io/en/latest/advanced_csv_import.html#links-csv
     merged.columns = ["officer_ids", "url"]
@@ -37,7 +45,12 @@ def main(id_path: Path, link_path: Path, output: Path):
     merged.loc[_not_divest, "author"] = "nbd1232"
     # Add an empty id column
     merged["id"] = None
+    missing_output = output.parent / f"{output.stem}__missing.csv"
+    log.info(f"Writing {len(missing)} missing records to {missing_output}")
+    missing.to_csv(missing_output, index=False)
+    log.info(f"Writing {len(merged)} output records to {output}")
     merged.to_csv(output, index=False)
+    log.info("Finished")
 
 
 @click.command()
@@ -45,6 +58,10 @@ def main(id_path: Path, link_path: Path, output: Path):
 @click.argument("link_path", type=click.Path(exists=True, path_type=Path))
 @click.argument("output", type=click.Path(exists=True, path_type=Path))
 def cli(id_path: Path, link_path: Path, output: Path):
+    logging.basicConfig(
+        format="[%(asctime)s - %(name)s - %(lineno)3d][%(levelname)s] %(message)s",
+        level=logging.INFO,
+    )
     main(id_path, link_path, output)
 
 
