fd58487c269941944a1ceff66dbe464f93200838
Madison Swain-Bowden <bowdenm@spu.edu>
2021-09-03 22:49:39 -0700

    Documentation!

--- data-munging/spd_2020_salary_data.py
+++ data-munging/spd_2020_salary_data.py
@@ -20,11 +20,16 @@ def main(id_path: Path, data: Path, output: Path):
     ids.columns = ["id", "last", "first"]
     # Remove Jr, IV, II, III, etc.
     df.loc[:, "Name"] = df.Name.replace(r" ?((Jr)|(II)|(III)|(IV))\.?", "", regex=True)
+    # Split the name by comma, the first part is the last name
     df["last"] = df.Name.str.split(",").str[0]
+    # The second part is the first name, but we only want the text *before* the
+    # first space, because the rest includes the middle initial which we can ignore
     df["first"] = df.Name.str.split(",").str[1].str.split(" ").str[0]
+    # Merge with prod data based on the first and last names
     merged = df.merge(ids, how="left", on=["last", "first"]).astype(
         {"id": pd.Int64Dtype()}
     )
+    # Reduce to a subset of the columns
     merged = merged[["Name", "Base Pay", "Overtime", "first", "last", "id"]]
     # Split off the links that don't have an OpenOversight badge associated with them
     _has_id = merged["id"].notna()
@@ -33,14 +38,22 @@ def main(id_path: Path, data: Path, output: Path):
     log.info(f"Writing {len(missing)} missing records to {missing_output}")
     missing.to_csv(missing_output, index=False)
     merged = merged[_has_id]
+    # Reduce columns even more
     merged = merged[["Base Pay", "Overtime", "id"]]
+    # Rename columns for OO
     merged.columns = ["salary", "overtime_pay", "officer_id"]
+    # Add an empty id column
     merged["id"] = None
+    # Set the year to 2020
     merged["year"] = 2020
+    # Only save out records with an OO ID
     merged = merged[~merged.officer_id.isna()]
+    # Convert the salary from "$###,###.##" to a float
     merged.loc[:, "salary"] = (
         merged["salary"].replace("[\$,]", "", regex=True).astype(float)
     )
+    # Do the same for overtime pay, but overtime pay may also be null
+    # so we have to do some special replacements so the float conversion works
     merged.loc[:, "overtime_pay"] = (
         merged["overtime_pay"]
         .replace("[\$, ]", "", regex=True)
