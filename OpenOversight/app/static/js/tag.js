b79f342609075493dfae5797e563661800cec09a
sea-kelp <66500457+sea-kelp@users.noreply.github.com>
2022-11-01 23:58:01 -0700

    Fix frame sizing issue and handle untagged images

--- OpenOversight/app/static/js/tag.js
+++ OpenOversight/app/static/js/tag.js
@@ -1,26 +1,40 @@
+function isNumeric(value) {
+    return /^\d+$/.test(value)
+}
+
 $(document).ready(function () {
     const img = $("#face-img")
     const frame = $("#face-tag-frame")
 
-    // To prevent hi-res images from taking over the entire page, the image is being shown
-    // in a responsive element. This means we need to compute the tag frame dimensions using
-    // percentages so it will scale if the page size changes.
+    const frameWidth = img.data("width")
+    const frameHeight = img.data("height")
+    const frameLeft = img.data("left")
+    const frameTop = img.data("top")
 
-    // To do this, we're dividing the tag dimensions by the image's actual width/height and
-    // multiplying by 100.
-    const tagWidth = parseInt(img.data("width")) / img[0].naturalWidth * 100
-    const tagHeight = parseInt(img.data("height")) / img[0].naturalHeight * 100
-    const tagLeft = parseInt(img.data("left")) / img[0].naturalWidth * 100
-    const tagTop = parseInt(img.data("top")) / img[0].naturalHeight * 100
+    if ([frameWidth, frameHeight, frameLeft, frameTop].every(isNumeric)) {
+        img.on("load", function () {
 
-    frame.css({
-        height: tagHeight + "%",
-        width: tagWidth + "%",
-        left: tagLeft + "%",
-        top: tagTop + "%",
-        visibility: "visible",
-    })
+            // To prevent hi-res images from taking over the entire page, the image is being shown
+            // in a responsive element. This means we need to compute the tag frame dimensions
+            // using percentages so it will scale if the page size changes.
 
-    // Make sure wrapper does not expand larger than image
-    $(".face-wrap").css("maxWidth", img[0].naturalWidth + "px")
+            // To do this, we're dividing the tag dimensions by the image's actual width/height and
+            // multiplying by 100.
+            const tagWidth = parseInt(frameWidth) / img[0].naturalWidth * 100
+            const tagHeight = parseInt(frameHeight) / img[0].naturalHeight * 100
+            const tagLeft = parseInt(frameLeft) / img[0].naturalWidth * 100
+            const tagTop = parseInt(frameTop) / img[0].naturalHeight * 100
+
+            frame.css({
+                height: tagHeight + "%",
+                width: tagWidth + "%",
+                left: tagLeft + "%",
+                top: tagTop + "%",
+                visibility: "visible",
+            })
+
+            // Make sure wrapper does not expand larger than image
+            $(".face-wrap").css("maxWidth", img[0].naturalWidth + "px")
+        });
+    }
 });
